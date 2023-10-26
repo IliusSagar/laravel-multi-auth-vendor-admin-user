bb9e65f8862319557598ae92bb0dbc67e52d5ec5
iliussagar <sagar01830@gmail.com>
2023-10-27 01:25:41 +0600

    breeze install webpack mix old version

---
+++ resources/views/Note.blade.php
@@ -0,0 +1,14 @@
+composer require laravel/breeze --dev
+php artisan breeze:install
+php artisan migrate 
+npm install 
+npm run dev
+
+css problem issues:
+composer require laravel/breeze:1.9.4
+php artisan breeze:install
+npm install 
+npm run dev
+npm install autoprefixer@10.4.5 --save-exact
+npm run dev
+----------------------------------------------
